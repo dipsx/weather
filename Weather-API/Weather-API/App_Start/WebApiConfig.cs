8eec5b5290cec8993b8e03f8bb8d7198dbc4ed05
Daniel Zhu <dzhu2005@gmail.com>
2023-05-28 21:23:51 -0400

    dev

---
+++ Weather-API/Weather-API/App_Start/WebApiConfig.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Web.Http;
+
+namespace Weather_API
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+
+            var formatters = GlobalConfiguration.Configuration.Formatters;
+            formatters.Remove(formatters.XmlFormatter);
+
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("application/json"));
+
+
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
---
+++ Weather-API/Weather-API/Controllers/WeatherController.cs
@@ -0,0 +1,27 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace Weather_API.Controllers
+{
+    public class WeatherController : ApiController
+    {
+
+        public string Get(string city)
+        {
+            string url = $"https://goweather.herokuapp.com/weather/{city}";
+
+            RestSharp.RestClient client = new RestSharp.RestClient("https://goweather.herokuapp.com");
+            RestSharp.RestRequest request = new RestSharp.RestRequest($"weather/{city}", method: RestSharp.Method.Get);
+
+            var response = client.Execute(request);
+            return response.Content;
+        }
+
+    }
+}
