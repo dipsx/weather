35784a2639793bd1c5943a9816985972157fb6f6
Daniel Zhu <dzhu2005@gmail.com>
2023-05-28 22:01:07 -0400

    dev

--- Weather-API/Weather-API/Controllers/WeatherController.cs
+++ Weather-API/Weather-API/Controllers/WeatherController.cs
@@ -10,18 +10,7 @@ using System.Web.Mvc;
 namespace Weather_API.Controllers
 {
     public class WeatherController : ApiController
-    {
-
-        public string Get(string city)
-        {
-            string url = $"https://goweather.herokuapp.com/weather/{city}";
-
-            RestSharp.RestClient client = new RestSharp.RestClient("https://goweather.herokuapp.com");
-            RestSharp.RestRequest request = new RestSharp.RestRequest($"weather/{city}", method: RestSharp.Method.Get);
-
-            var response = client.Execute(request);
-            return response.Content;
-        }
+    {        
 
     }
 }
